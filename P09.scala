b92ebe71d41808b91e06dd30dab82fb9521d94db
Kitae Lee <vivasoul@naver.com>
2018-01-14 23:55:58 +0900

    P09 solution update

---
+++ P09.scala
@@ -0,0 +1,19 @@
+/*
+Pack consecutive duplicates of list elements into sublists.
+If a list contains repeated elements they should be placed in separate sublists.
+Example:
+
+scala> pack(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
+res0: List[List[Symbol]] = List(List('a, 'a, 'a, 'a), List('b), List('c, 'c), List('a, 'a), List('d), List('e, 'e, 'e, 'e))
+ */
+object P09 {
+  def pack[A](xs:List[A]):List[List[A]] = {
+    def packR[B](ys:List[B]):List[List[B]] = ys match {
+      case h :: t => ys.takeWhile(_ == h) :: packR(ys.dropWhile(_ == h))
+      case Nil => Nil
+    }
+    packR(xs)
+  }
+}
+P09.pack(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
+
